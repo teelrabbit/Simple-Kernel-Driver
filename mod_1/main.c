5a62a0efb94fb3e62e55994d7558fef722204d53
teelrabbit <xuntitled@protonmail.com>
2022-02-19 10:54:47 -0800

    update

--- mod_1/main.c
+++ mod_1/main.c
@@ -12,7 +12,7 @@ static char *mystring; module_param(mystring, charp, 0);
 static unsigned long *__sys_call_table = NULL; //null=0
 
 struct file_operations fops = {
-    read: device_read
+    read: device_read,
    //write: device_write
     //open: device_open,
     //release: device_releae
