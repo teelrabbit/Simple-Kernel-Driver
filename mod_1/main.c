4bdf66aeae2d3d24c257dcee5ccc738daeff0a76
teelrabbit <xuntitled@protonmail.com>
2022-02-19 11:03:56 -0800

    update

--- mod_1/main.c
+++ mod_1/main.c
@@ -11,12 +11,8 @@ MODULE_VERSION("0.0.4");
 static char *mystring; module_param(mystring, charp, 0);
 static unsigned long *__sys_call_table = NULL; //null=0
 
-struct file_operations fops = {
-    read: device_read,
-   //write: device_write
-    //open: device_open,
-    //release: device_releae
-};
+// creating buffer for proc file
+struct proc_dir_entery *Our_Proc_File;
 
 static int modInit(void) {
 
